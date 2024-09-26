268e36f518ea1034c548edb77ee922b9a5c4a4a9
Matthew Eshleman <matthew@covemountainsoftware.com>
2024-09-26 15:48:37 -0500

    Initial empty build

---
+++ qassert-meta-lib/include/qassert-meta.h
@@ -0,0 +1,15 @@
+#ifndef QASSERT_META_QASSERT_META_H
+#define QASSERT_META_QASSERT_META_H
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //QASSERT_META_QASSERT_META_H
---
+++ qassert-meta-lib/tests/qassert-meta-tests.cpp
@@ -0,0 +1,16 @@
+#include "CppUTest/TestHarness.h"
+#include "qassert-meta.h"
+
+
+TEST_GROUP(qassert_meta_tests) {
+    void setup() final
+    {
+    }
+    void teardown() final
+    {
+    }
+};
+
+TEST(qassert_meta_tests, can_compile)
+{
+}
