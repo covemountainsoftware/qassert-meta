abd9180a235cac7b9b84cf78fd1b35a7b50f50d2
Matthew Eshleman <matthew@covemountainsoftware.com>
2024-09-28 16:26:34 -0500

    qf_actq:202, 201, 310, 400

--- qassert-meta-lib/src/qassert-meta.c
+++ qassert-meta-lib/src/qassert-meta.c
@@ -56,7 +56,18 @@ bool QAssertMetaGetDescription(const char * module, int id, QAssertMetaDescripti
 
 static const char * const QPC_URL_QACTIVE_POST =
         "https://www.state-machine.com/qpc/struct_q_active.html#a1a81b9fd06d9c0aa5dea32d194f0552b";
+static const char * const QPC_URL_QACTIVE_POST_LIFO =
+        "https://www.state-machine.com/qpc/struct_q_active.html#a3a129f47e6d0c75c11a8902f8137f007";
 
+
+
+/**
+ * Collection of QP/QF related asserts.
+ *
+ * These are currently the opinion of Matthew Eshleman only
+ * and are not necessarily the official description
+ * of any particular QASSERT.
+ */
 static InternalMeta m_internal[] = {
     {
         "qf_actq", 102,
@@ -79,8 +90,43 @@ static InternalMeta m_internal[] = {
             QPC_URL_QACTIVE_POST
         }
     },
-
-    //keep this last, terminating structure
+    {
+        "qf_actq", 202,
+        {
+            "The event being LIFO posted is either null, invalid, or corrupt.",
+            "This typically means the event pointer was improperly retained\n"
+            "after the event was returned to its event pool,\n"
+            "i.e. used after the event was garbage collected.",
+            QPC_URL_QACTIVE_POST_LIFO
+        }
+    },
+    {
+        "qf_actq", 201,
+        {
+            "The target active object's queue is full.",
+            "If LIFO posting an event to an active object, and the target queue is full,\n"
+            "this assert will occur. The target AO might be overloaded OR a higher priority AO might\n"
+            "be preventing this AO from executing.",
+            QPC_URL_QACTIVE_POST_LIFO
+        }
+    },
+    {
+        "qf_actq", 310,
+        {
+            "The final event was removed from the queue, yet internal data indicate a fault.",
+            "Possible data corruption.",
+            NULL
+        }
+    },
+    {
+        "qf_actq", 400,
+        {
+            "Returns the minimum of free queue entries of a target active object, using the priority as the key.",
+            "Most likely, a currently unused priority was queried.",
+            "https://www.state-machine.com/qpc/class_q_f.html#a29692c0dcab731199b5beb5847484ab7"
+        }
+    },
+    //List terminating structure, keep last.
     {
         NULL, -1, {NULL, NULL, NULL}
     }
